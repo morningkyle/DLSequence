336a5302752bed9675bcc28f22027d63da7e57c6
kylezou <31173219+morningkyle@users.noreply.github.com>
2018-02-25 11:22:13 +0800

    Change optimizer from sgd to adam, the latter one has much better convergence performance

--- SimpleRNN.py
+++ SimpleRNN.py
@@ -2,25 +2,26 @@ from keras.models import Sequential
 from keras.layers import SimpleRNN, Dense, Activation
 from keras import optimizers
 from keras.utils import plot_model
-import numpy as np
+from keras.callbacks import CSVLogger, ModelCheckpoint
 
 from data_funcs import rnn_test_data
+from MyCallback import MyCallback
 
 
 '''
 Generate test data from a real simple RNN model, and then use these data to train a new simple
 RNN network again. Observed conclusion:
 (1) With preset weights, the model could predict accurate results without training. So we may
-    know the best result before training.
-(2) When the input data is large (> 1), the training effect is bad. The training tend to fail
-    even when the initialized weights are already the best ones. Need to find out why next.
+    know the best parameters before training.
+(2) When the input data is large (> 1), the training effect is bad. This is because the output
+    of each layer is very easy to saturate, and the accuracy of float32 is lost.
 '''
 
 # Build test data
 samples = 100
 u = 0.01
 v = 0.01
-w = 100
+w = 10
 xt, yt = rnn_test_data(samples, u=u,  v=v, w=w)
 x_train = xt.reshape(samples, 1, 1)
 y_train = yt.reshape(samples, 1)
@@ -31,7 +32,8 @@ print("y_train: ", yt)
 model = Sequential()
 model.add(SimpleRNN(1, batch_input_shape=(1, 1, 1), stateful=True))
 model.add(Dense(1))
-model.compile(optimizer="sgd", loss="mean_squared_error")
+# ADAM optimizer has much better performance than SGD in this model
+model.compile(optimizer="adam", loss="mean_squared_error", metrics=['accuracy'])
 
 weights = model.get_weights()
 # Manually initialize weights
@@ -42,17 +44,21 @@ model.set_weights(weights)
 print(weights)
 
 # Start training
-# FIXME: with above input data, the more data we used to train, the bad training result we have.
-# FIXME: not sure why???
-history = model.fit(x=x_train[0:3], y=y_train[0:3], batch_size=1, epochs=1, shuffle=False, verbose=2)
+checkpoint = CSVLogger('log.csv', append=True, separator=';')
+# checkpoint = ModelCheckpoint(filepath='weights.{epoch:02d}.hdf5', verbose=1)
+# checkpoint = MyCallback()
+for i in range(3):
+    history = model.fit(x=x_train, y=y_train, batch_size=1, epochs=1, verbose=2,
+                        callbacks=[checkpoint], shuffle=False)
+    model.reset_states()
 print(model.get_weights())
 
 print("========================================================================")
 # samples-10
-y_predict = model.predict(x=x_train[0:samples], batch_size=1)
+y_predict = model.predict(x=x_train[samples-10:samples], batch_size=1)
 
 # Print predicted and target value
-for a, b in zip(y_predict, y_train[0:samples]):
+for a, b in zip(y_predict, y_train[samples-10:samples]):
     print('predicted: ', a, 'target: ', b)
 
 # Print model summary
