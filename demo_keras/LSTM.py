b95f9ac7109e5806176a2a9e1c577d13e65df932
kylezou <31173219+morningkyle@users.noreply.github.com>
2018-03-13 17:45:57 +0800

    Code refactorying

--- demo_keras/LSTM.py
+++ demo_keras/LSTM.py
@@ -3,12 +3,11 @@ from keras.layers import LSTM, Dense, Activation
 import numpy as np
 
 
-'''
-Smaller values are easier to getter better training result(eg. less epochs needed).
-'''
-
 def init_test_sequence(length):
-    seq = np.array([i for i in range(length)])
+    """
+        Generate a test sequence. Smaller values could converge with much less epochs.
+    """
+    seq = np.array([i/10 for i in range(length)])
     print("test sequence: ", seq)
     x = seq.reshape(length, 1, 1)
     y = seq.reshape(length, 1)
@@ -20,20 +19,24 @@ def create_model(n_neurons):
     model = Sequential()
     model.add(LSTM(n_neurons, input_shape=(1, 1)))
     model.add(Dense(1))
-    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
-    print(model.summary())
+    model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
     return model
 
 
-# define LSTM configuration
-length = 10
-xt, yt = init_test_sequence(length)
-model = create_model(3)
-# Train LSTM
-model.fit(xt, yt, epochs=10000, batch_size=length, verbose=2)
-# Evaluate
-result = model.predict(xt, batch_size=length, verbose=0)
-for a, b in zip(result, yt):
-    print('predicted: ', a, 'target: ', b)
+def main():
+    # define LSTM configuration
+    length = 10
+    xt, yt = init_test_sequence(length)
+    model = create_model(3)
+    # Train LSTM
+    model.fit(xt, yt, epochs=10000, batch_size=length, verbose=2)
+    # Evaluate
+    result = model.predict(xt, batch_size=length, verbose=0)
+    for a, b in zip(result, yt):
+        print('predicted: ', a, 'target: ', b)
 
-print(model.summary())
+    print(model.summary())
+
+
+if __name__ == "__main__":
+    main()
--- demo_keras/SimpleRNN.py
+++ demo_keras/SimpleRNN.py
@@ -1,3 +1,11 @@
+"""
+Generate test data from a real simple RNN model, and then use these data to train a new simple RNN
+network again. Observed conclusion:
+(1) With preset weights, the model could predict accurate results without training. So we may know
+    the best parameters before training.
+(2) When the input data is large (> 1), the training effect is bad. This is because the output of
+    each layer is very easy to saturate, and the accuracy of float32 is lost.
+"""
 from keras.models import Sequential
 from keras.layers import SimpleRNN, Dense
 from keras.utils import plot_model
@@ -5,27 +13,19 @@ from keras.callbacks import CSVLogger
 
 from data_funcs import rnn_test_data
 
-'''
-Generate test data from a real simple RNN model, and then use these data to train a new simple
-RNN network again. Observed conclusion:
-(1) With preset weights, the model could predict accurate results without training. So we may
-    know the best parameters before training.
-(2) When the input data is large (> 1), the training effect is bad. This is because the output
-    of each layer is very easy to saturate, and the accuracy of float32 is lost.
-'''
 
 # Build test data
-samples = 100
 u = 0.01
 v = 0.01
 w = 10
+samples = 100
 xt, yt = rnn_test_data(samples, u=u,  v=v, w=w)
 x_train = xt.reshape(samples, 1, 1)
 y_train = yt.reshape(samples, 1)
 print("x_train: ", xt)
 print("y_train: ", yt)
 
-# Build simple RNN model
+# Build simple a RNN model
 model = Sequential()
 model.add(SimpleRNN(1, batch_input_shape=(1, 1, 1), stateful=True))
 model.add(Dense(1))
@@ -33,7 +33,7 @@ model.add(Dense(1))
 model.compile(optimizer="adam", loss="mean_squared_error", metrics=['accuracy'])
 
 weights = model.get_weights()
-# Manually initialize weights
+# Demo how to manually initialize weights for this model
 weights[0][0, 0] = u
 weights[1][0, 0] = v
 weights[3][0, 0] = w
@@ -45,13 +45,13 @@ checkpoint = CSVLogger('log.csv', append=True, separator=';')
 # checkpoint = ModelCheckpoint(filepath='weights.{epoch:02d}.hdf5', verbose=1)
 # checkpoint = MyCallback()
 for i in range(3):
-    history = model.fit(x=x_train, y=y_train, batch_size=1, epochs=1, verbose=2,
+    history = model.fit(x=x_train[0:samples-10], y=y_train[0:samples-10], batch_size=1, epochs=1, verbose=2,
                         callbacks=[checkpoint], shuffle=False)
     model.reset_states()
 print(model.get_weights())
 
 print("========================================================================")
-# samples-10
+# Let's check our prediction performance
 y_predict = model.predict(x=x_train[samples-10:samples], batch_size=1)
 
 # Print predicted and target value
